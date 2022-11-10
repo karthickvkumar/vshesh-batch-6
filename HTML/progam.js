6854d19810899da0ffe2b81312eee97d48571e10
kvelraj_concord <kvelraj@concord.net>
2022-11-10 10:21:09 +0530

    js

---
+++ HTML/progam.js
@@ -0,0 +1,18 @@
+// Number - Any numerical values. It can be Positive, Negative, Fractional, Decimal value
+
+// String - Any Text value. It can be A-Z, a-z, 0-9, special character (@#$%). It should be enclosed in Single or Double Quote ('', "")
+
+// Boolean - Either true or false
+
+// null - Non Existance of value
+
+// undefined - Value is not yet definied. 
+
+// Variable Declaration 
+// syntax:
+// var variable_name = value; 
+// variable_name - we need to provide a meaningful name, It should contains A-Z, a-z, 0-9, _ , $
+// value - Number, String, Boolean, null, undefined
+
+var username = "Karthick Kumar";
+var user_age = 28;
