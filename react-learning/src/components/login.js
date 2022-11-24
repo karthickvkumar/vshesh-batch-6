4975d152ad6633d3695de1418c1f52dd7df5cf18
kvelraj_concord <kvelraj@concord.net>
2022-11-24 10:46:08 +0530

    image

--- react-learning/src/components/login.js
+++ react-learning/src/components/login.js
@@ -14,6 +14,10 @@ const LoginPage = () => {
       <div className="space">
         <button className="button">Login</button>
       </div>
+      <div>
+        <img src={require("../images/shinchu.jpg")} width="300" />
+        <img src="https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=960" width="300" />
+      </div>
     </div>
   )
 }
