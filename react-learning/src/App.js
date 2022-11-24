ecf80d1558f96bd87014a952f281a1609b30cc3b
kvelraj_concord <kvelraj@concord.net>
2022-11-24 10:18:19 +0530

    css code

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import LoginPage from "./components/login";
 
+import "./App.css";
+
 const App = () => {
   return(
     <div>
--- react-learning/src/components/login.js
+++ react-learning/src/components/login.js
@@ -3,16 +3,16 @@ import React from "react";
 const LoginPage = () => {
   return(
     <div>
-      <div>
-        <label>Enter your Email ID</label>
-        <input type="text" placeholder="Please enter email id" />
+      <div className="space">
+        <label className="align-label">Enter your Email ID</label>
+        <input type="text" className="align-input" placeholder="Please enter email id" />
       </div>
-      <div>
-        <label>Enter your Password</label>
-        <input type="password" placeholder="Please enter password" />
+      <div className="space">
+        <label className="align-label">Enter your Password</label>
+        <input type="password" className="align-input" placeholder="Please enter password" />
       </div>
-      <div>
-        <button>Login</button>
+      <div className="space">
+        <button className="button">Login</button>
       </div>
     </div>
   )
