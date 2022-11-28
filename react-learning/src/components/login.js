8e0771b4901ddf87723d82b064ce1981748a4439
kvelraj_concord <kvelraj@concord.net>
2022-11-28 10:09:39 +0530

    use state

--- react-learning/src/components/login.js
+++ react-learning/src/components/login.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 const LoginPage = () => {
+
+  const [formValue, setFormValue] = useState({
+    email_id : "",
+    password : ""
+  });
 
   let greetings = "Welcome to Login Page";
   let studentInfo = {
@@ -10,23 +15,26 @@ const LoginPage = () => {
 
   const onLoginAction = () => {
     alert("This is working");
+    console.log(formValue);
   }
 
   const onHandleInput = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value, event.target.name);
+    setFormValue({...formValue, [event.target.name] : event.target.value});
   }
 
   return(
     <div>
       <h1>{greetings}</h1>
       <h1>The student name is {studentInfo.name}, and he is {studentInfo.age} years old</h1>
+      <h1>Your email id is {formValue.email_id} and password {formValue.password}</h1>
       <div className="space">
         <label className="align-label">Enter your Email ID</label>
-        <input type="text" className="align-input" placeholder="Please enter email id" onChange={onHandleInput} />
+        <input type="text" className="align-input" placeholder="Please enter email id" onChange={onHandleInput} name="email_id" />
       </div>
       <div className="space">
         <label className="align-label">Enter your Password</label>
-        <input type="password" className="align-input" placeholder="Please enter password" onChange={onHandleInput} />
+        <input type="password" className="align-input" placeholder="Please enter password" onChange={onHandleInput} name="password" />
       </div>
       <div className="space">
         <button className="button" onClick={() => onLoginAction()}>Login</button>
