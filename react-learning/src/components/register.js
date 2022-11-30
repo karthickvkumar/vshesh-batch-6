81acd629faf472c76be382812d2a818b13af492e
kvelraj_concord <kvelraj@concord.net>
2022-11-30 10:19:04 +0530

    register page

--- react-learning/src/components/register.js
+++ react-learning/src/components/register.js
@@ -1,40 +1,77 @@
-import React from "react";
+import React, {useState} from "react";
 
 const RegisterPage = () => {
+
+  const [registerForm, setRegisterForm] = useState({
+    username : "",
+    password : "",
+    date_of_birth: "",
+    gender: "",
+    skills : "",
+    educational_degree: "",
+    address : ""
+  });
+
+  const createNewAccount = () => {
+    alert("Your account is created");
+    console.log(registerForm);
+  }
+
+  const onHandleInput = (event) => {
+    // console.log(event.target.value)
+    setRegisterForm({...registerForm, [event.target.name] : event.target.value });
+  }
+
   return(
     <div>
       <h1>Register Page</h1>
       <div className="space">
         <label className="align-label">Enter your UserName</label>
-        <input type="text" className="align-input" placeholder="Please enter username"/>
+        <input type="text" className="align-input" placeholder="Please enter username" onChange={onHandleInput} name="username"/>
       </div>
       <div className="space">
         <label className="align-label">Enter your Password</label>
-        <input type="password" className="align-input" placeholder="Please enter password"/>
+        <input type="password" className="align-input" placeholder="Please enter password" onChange={onHandleInput} name="password"/>
       </div>
       <div className="space">
         <label className="align-label">Select your Date of Birth</label>
-        <input type="date" className="align-input"/>
+        <input type="date" className="align-input" onChange={onHandleInput} name="date_of_birth"/>
       </div>
       <div className="space">
         <label className="align-label">Select your Gender</label>
-        <input type="radio" name="gender"/><span className="font">Male</span>
-        <input type="radio" name="gender"/><span className="font">Female</span>
-        <input type="radio" name="gender"/><span className="font">Others</span>
+        <input type="radio" name="gender" onChange={onHandleInput} value="Male"/>
+        <span className="font">Male</span>
+        <input type="radio" name="gender" onChange={onHandleInput} value="Female"/>
+        <span className="font">Female</span>
+        <input type="radio" name="gender" onChange={onHandleInput} value="Others"/>
+        <span className="font">Others</span>
       </div>
       <div className="space">
         <label className="align-label">Select your IT Skills</label>
-        <input type="checkbox"/><span className="font">JavaScript</span>
-        <input type="checkbox"/><span className="font">Python</span>
-        <input type="checkbox"/><span className="font">C# .NET</span>
-        <input type="checkbox"/><span className="font">Angular</span>
-        <input type="checkbox"/><span className="font">React JS</span>
-        <input type="checkbox"/><span className="font">PHP</span>
-        <input type="checkbox"/><span className="font">Core JAVA</span>
+        <input type="checkbox" onChange={onHandleInput} value="JavaScript" name="skills"/>
+        <span className="font">JavaScript</span>
+
+        <input type="checkbox" onChange={onHandleInput} value="Python" name="skills"/>
+        <span className="font">Python</span>
+
+        <input type="checkbox" onChange={onHandleInput} value="C# .NET" name="skills"/>
+        <span className="font">C# .NET</span>
+
+        <input type="checkbox" onChange={onHandleInput} value="Angular" name="skills"/>
+        <span className="font">Angular</span>
+
+        <input type="checkbox" onChange={onHandleInput} value="React JS" name="skills"/>
+        <span className="font">React JS</span>
+
+        <input type="checkbox" onChange={onHandleInput} value="PHP" name="skills"/>
+        <span className="font">PHP</span>
+
+        <input type="checkbox" onChange={onHandleInput} value="Core JAVA" name="skills"/>
+        <span className="font">Core JAVA</span>
       </div>
       <div className="space">
         <label className="align-label">Select your Degree</label>
-        <select className="align-input">
+        <select className="align-input" onChange={onHandleInput} name="educational_degree">
           <option>Select your qualification</option>
           <option>B.E Computer Science</option>
           <option>B.Tech IT</option>
@@ -46,10 +83,10 @@ const RegisterPage = () => {
       </div>
       <div className="space">
         <label className="align-label">Enter your Address</label>
-        <textarea className="align-input"></textarea>
+        <textarea className="align-input" onChange={onHandleInput} name="address"></textarea>
       </div>
       <div className="space">
-        <button className="button">Create your Account</button>
+        <button className="button" onClick={() => createNewAccount()}>Create your Account</button>
       </div>
     </div>
   )
