9775a2bd1c259557973cd8f0899a523c3580a999
kvelraj_concord <kvelraj@concord.net>
2022-12-05 10:30:10 +0530

    api

--- react-learning/src/components/inbox.js
+++ react-learning/src/components/inbox.js
@@ -1,9 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 const InboxPage = () => {
+
+  const loadUsers = () => {
+    let url = "https://reqres.in/api/users?page=2";
+
+    axios.get(url)
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+  }
+
   return(
     <div>
       <h1>Inbox Page is loaded</h1>
+      <button onClick={() => loadUsers()}>Load List of Users</button>
     </div>
   )
 }
