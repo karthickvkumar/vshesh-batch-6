3184b87dbcd09b7d43309ae71a7b63c7564789c5
kvelraj_concord <kvelraj@concord.net>
2022-12-07 10:46:41 +0530

    delete api

--- react-learning/src/components/trash.js
+++ react-learning/src/components/trash.js
@@ -1,9 +1,25 @@
 import React from "react";
+import axios from "axios";
 
 const TrashPage = () => {
+
+  const deleteUsers = () => {
+    const url = "https://reqres.in/api/users/2";
+
+    axios.delete(url)
+        .then((response) => {
+          console.log(response);
+          alert("User profile has been deleted");
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+  }
+
   return(
     <div>
       <h1>Trash Page is loaded</h1>
+      <button onClick={() => deleteUsers()}>Delete Users</button>
     </div>
   )
 }
