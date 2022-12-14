552faad3322a4f5cca2a314ed7009fcd0f38110d
kvelraj_concord <kvelraj@concord.net>
2022-12-14 10:50:21 +0530

    get api

--- node-api/server.js
+++ node-api/server.js
@@ -11,6 +11,44 @@ app.use(cors({
   origin: "*"
 }));
 
+var connection = mysql.createConnection({
+  host : "remotemysql.com",
+  user : "wym4khPjwJ",
+  password: "IpVePeo1GV",
+  database: "wym4khPjwJ",
+  port: 3306
+});
+// XAMMP
+// host : "localhost",
+// user : "root",
+// password : "",
+// database : "your db name"
+
+
+connection.connect((error) => {
+  if(error){
+    throw error;
+  }
+  else{
+    console.log("MYSQL Database is connected successfully");
+  }
+});
+
+http://localhost:4000/list
+app.get("/list", (request, response) => {
+  var sqlQuery = `SELECT * FROM karthick_kumar`;
+
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+    }
+    else{
+      response.status(200).send(result);
+    }
+  })
+
+})
+
 
 
 var port = process.env.PORT || 4000;
